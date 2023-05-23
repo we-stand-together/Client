d1f7787cd8011ba83e5c76add15f3a6f1738722d
Ziv Caspi <caspifx@gmail.com>
2023-05-23 12:49:45 +0300

    setup router

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from './pages/Login';
+import Main from './pages/Main';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Itamr Lahav  Is A Nehfaf Hadash</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='login'>
+        <Stack.Screen name="login" component={Login}/>
+        <Stack.Screen name="main" component={Main}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
--- pages/Login.tsx
+++ pages/Login.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import {View} from 'react-native';
-import {Title} from 'react-native-paper';
+import {Button, Title} from 'react-native-paper';
 import base from '../styles/base';
 
-interface LoginProps {}
+interface LoginProps {
+    navigation: any
+}
 
 const Login: React.FunctionComponent<LoginProps> = (props) => {
   return (
     <View style={base.centered}>
       <Title>Login</Title>
+      <Button onPress={() => {
+        props.navigation.navigate('main');
+      }}>Go To Main</Button>
     </View>
   );
 };

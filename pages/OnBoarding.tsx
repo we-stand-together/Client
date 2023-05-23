b290f8d79c49cde9994e4c82583a59935c572be7
Itamar <itamarlahav444>
2023-05-23 15:11:54 +0300

    Next button navigate to signup page

--- pages/OnBoarding.tsx
+++ pages/OnBoarding.tsx
@@ -1,22 +1,24 @@
-import {View} from 'react-native';
+import { View } from 'react-native';
 import React from "react";
-import {Button, Title, Text} from 'react-native-paper';
+import { Button, Title, Text } from 'react-native-paper';
 import base from '../styles/base';
 
-interface OnBoardingProps{
+interface OnBoardingProps {
     navigation: any
 }
 
 const OnBoarding: React.FunctionComponent<OnBoardingProps> = (props) => {
 
-  return (
-    <View style={base.centered}>
-      <Title>Welcome</Title>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
-      <Text>We Stand Together is an app for family members, freinds, colleagues, and people that want to help victims of domestic abuse and to help those suffering from domestic abuse themselves</Text>
-      <Button onPress={() => {
-        props.navigation.navigate('main');
-      }}>Next</Button>
-    </View>
-  );
+    const handleNextPage = () => {
+        props.navigation.navigate('signup');
+    }
+
+    return (
+        <View style={base.centered}>
+            <Title>Welcome</Title>
+            <Text>We Stand Together is an app for family members, freinds, colleagues, and people that want to help victims of domestic abuse and to help those suffering from domestic abuse themselves</Text>
+            <Button onPress={handleNextPage}>Next</Button>
+        </View>
+    );
 };
 export default OnBoarding;
